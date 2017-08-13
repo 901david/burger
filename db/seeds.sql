851c13a16d7471e510d455d9a04a2b7b503a1397
DHLAP\h <vdavidhammond@gmail.com>
2017-08-12 19:53:06 -0600

    Adding in file structure setting up db and establishing a connection

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers SET burger_name='Bacon Cheeseburger', devoured=false;
+INSERT INTO burgers SET burger_name='Bleu Cheeseburger', devoured=false;
+INSERT INTO burgers SET burger_name='BBQ Onion Fry Burger', devoured=false;
+INSERT INTO burgers SET burger_name='Veggie Burger', devoured=false;
+INSERT INTO burgers SET burger_name='BlackBean Chili Cheeseburger', devoured=false;
